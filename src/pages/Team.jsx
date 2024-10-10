9036b641e0748ec851010e45dc59fe29c26213f9
genfour <genfour99@gmail.com>
2024-10-10 07:29:33 +0530

    updated

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -69,7 +69,10 @@ export default function Team() {
           </div>
         </div>
         <div className="quote-container w-100 d-flex flex-row">
-          <div className="quote-left-container d-flex p-5 justify-content-start align-items-center">
+          <div
+            className="wow fadeInUp quote-left-container d-flex p-5 justify-content-start align-items-center"
+            data-bs-wow-delay="0.1s"
+          >
             <h1 className="text-start">
               "Coming together is the beginning;
               <br />
@@ -78,13 +81,19 @@ export default function Team() {
               <br />- Henry Ford
             </h1>
           </div>
-          <div className="quote-right-container"></div>
+          <div
+            className="wow fadeInUp quote-right-container"
+            data-bs-wow-delay="0.2s"
+          ></div>
         </div>
         <div className="staff-container d-flex flex-column p-5 w-100 h-auto">
           <h1 className="wow fadeInUp text-start mb-4" data-bs-wow-delay="0.1s">
             <span style={{ color: "#fe3c66" }}>VXL</span> Education Sri Lanka
           </h1>
-          <div className="directors-container d-flex flex-column">
+          <div
+            className="wow fadeInUp directors-container d-flex flex-column"
+            data-bs-wow-delay="0.2s"
+          >
             <div className="directors-row d-flex flex-row">
               <div className="directors-left-container d-flex flex-row align-items-center justify-content-start position-relative">
                 <div
@@ -221,11 +230,18 @@ export default function Team() {
         </div>
 
         <div className="join-container d-flex flex-column p-5 w-100 h-auto">
-          <h1 class="display-3 mb-3 text-start" style={{ color: "#0a4c7e" }}>
+          <h1
+            class="wow fadeInUp display-3 mb-3 text-start"
+            style={{ color: "#0a4c7e" }}
+            data-bs-wow-delay="0.1s"
+          >
             Want to be a part of{" "}
             <span style={{ color: "#fe3c66" }}>excellence?</span>
           </h1>
-          <p className="text-start hero-para">
+          <p
+            className="wow fadeInUp text-start hero-para"
+            data-bs-wow-delay="0.2s"
+          >
             Want to study abroad?{" "}
             <span style={{ color: "#fe3c66" }}>
               Having trouble deciding where or how?
@@ -235,7 +251,10 @@ export default function Team() {
             how you could be a part of ​one of Sri Lanka’s best study
             abroad/visa consulting teams!
           </p>
-          <button className="hero-button py-2 px-3 mt-2 me-auto">
+          <button
+            className="wow fadeInUp hero-button py-2 px-3 mt-2 me-auto"
+            data-bs-wow-delay="0.5s"
+          >
             Get in touch!
             <span className="bg-white rounded-circle ms-3 ">
               <FontAwesomeIcon
